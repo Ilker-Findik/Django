770e6bfeb9989d6317ba18931b40da07f5b24b46
Ilker-Proxy <ilkerfindik0@gmail.com>
2025-09-25 16:24:02 +0300

    1-Commit

--- shop/urunler/admin.py
+++ shop/urunler/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
-from .models import Kategoriler,Markalar
+from .models import Kategoriler, Markalar, Urunler, Etiketler
+
+
 # Register your models here.
 
 class KategoriAdmin(admin.ModelAdmin):
     list_display = ['isim','seo_title','slug','aktifMi']
     list_filter = ['aktifMi']
     search_fields = ['isim','seo_title']
-
 admin.site.register(Kategoriler,KategoriAdmin)
 
 class MarkalarAdmin(admin.ModelAdmin):
     list_display = ['isim','seo_title','slug','aktifMi','resim']
     list_filter = ['aktifMi']
     search_fields = ['isim','seo_title']
-
 admin.site.register(Markalar,MarkalarAdmin)
+
+class UrunlerAdmin(admin.ModelAdmin):
+    list_display = ['isim','seo_title','slug','aktifMi','resim','fiyat','tarih','indirimli_fiyat','marka']
+    list_filter = ['aktifMi','isim','kategori','marka']
+    search_fields = ['isim','seo_title','slug']
+admin.site.register(Urunler,UrunlerAdmin)
+
+admin.site.register(Etiketler)
--- shop/urunler/models.py
+++ shop/urunler/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import ForeignKey
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -32,6 +33,42 @@ class Markalar(models.Model):
     class Meta:
         verbose_name_plural = "Markalar"
         verbose_name = 'Marka'
+
+    def __str__(self):
+        return self.isim
+
+class Etiketler(models.Model):
+    isim = models.CharField(max_length=100)
+    aciklama = models.TextField(blank=True, null=True)
+    aktifMi = models.BooleanField(default=True)
+    slug = models.SlugField(max_length=100,unique=True,null=True,blank=True)
+    class Meta:
+        verbose_name_plural = "Etiketler"
+        verbose_name = 'Etiket'
+
+    def __str__(self):
+        return self.isim
+
+class Urunler (models.Model):
+    isim = models.CharField(max_length=100)
+    kullanici = models.ForeignKey(User, on_delete=models.CASCADE)
+    fiyat = models.DecimalField(decimal_places=2, max_digits=10)
+    marka =models.ForeignKey(Markalar, on_delete=models.CASCADE)
+    kategori = models.ForeignKey(Kategoriler, on_delete=models.CASCADE)
+    indirimli_fiyat = models.DecimalField(decimal_places=2, max_digits=10)
+    kisa_aciklama = models.TextField(blank=True,null=True)
+    aciklama = models.TextField(blank=True,null=True)
+    seo_title = models.CharField(max_length=100,blank=True,null=True)
+    seo_description = models.TextField(max_length=100,blank=True,null=True)
+    slug = models.SlugField(max_length=100,unique=True,null=True,blank=True)
+    aktifMi = models.BooleanField(default=True)
+    resim = models.ImageField(upload_to='urunresimleri',blank=True,null=True,)
+    tarih = models.DateTimeField(auto_now_add=True)
+    etiketler = models.ManyToManyField(Etiketler,blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Urunler'
+        verbose_name = 'Urun'
 
     def __str__(self):
         return self.isim
